21d2176af3affae1d686d0f481aa9f6d30a1f502
hseung578 <hseung578@gmail.com>
2022-12-10 17:59:58 +0900

    fix:fix getSales

--- mongodb/src/sales/sale.service.ts
+++ mongodb/src/sales/sale.service.ts
@@ -6,7 +6,9 @@ export class SaleService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    return await this.prisma.sales.findMany({ take: 5 });
+    return await this.prisma.sales.findMany({
+      take: 20,
+    });
   }
 
   async find(email: string) {
